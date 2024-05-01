4a37fc1a4171bfd5062367dbcbdef29339f6c987
CyberFather <maks12ate@gmail.com>
2024-05-01 16:22:59 +0300

    add: user table

--- pkg/db/queries.go
+++ pkg/db/queries.go
@@ -3,10 +3,19 @@ package db
 const scheme = `
 
 CREATE TABLE IF NOT EXISTS url (
-    name CHAR(5) PRIMARY KEY,
+    name CHAR(5),
     link TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    times INT DEFAULT 0
+    times INT DEFAULT 0,
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    isAdmin BOOLEAN DEFAULT False,
+    PRIMARY KEY (id)
 );
 
 `
